e091e4d57a58a621804ee0854173a23c76107f20
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2023-09-26 20:25:13 +0600

    dynamic features and Colors added on cards

--- src/Components/Header/Cards/Cards.jsx
+++ src/Components/Header/Cards/Cards.jsx
@@ -7,11 +7,11 @@ const Cards = ({dataForCards}) => {
 
     console.log(dataForCards)
     return (
-        <div className="pt-14 pb-20" >
-            <div className="py-16 px-24 grid grid-cols-1 md:grid-cols-2 lg:last:grid-cols-4 gap-8 ">
+        <div className="lg:pt-14 lg:pb-20" >
+            <div className="py-8 lg:py-16 px-2 lg:px-24 grid grid-cols-1 md:grid-cols-2 lg:last:grid-cols-4 gap-8 ">
                 {
                     // eslint-disable-next-line react/prop-types
-                    dataForCards?.map((eachCard)=><SingleCard key={eachCard.id}></SingleCard>)
+                    dataForCards?.map((eachCard)=><SingleCard key={eachCard.id} eachCard={eachCard}></SingleCard>)
                 }
             </div>
         </div>
--- src/Components/Header/Cards/SingleCard.jsx
+++ src/Components/Header/Cards/SingleCard.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 
 
-const SingleCard = () => {
+// eslint-disable-next-line react/prop-types
+const SingleCard = ({eachCard}) => {
+
+ console.log(eachCard);
+ const {id,category,img,title,cardColorBg,categoryBg,textColors} = eachCard || {}
+
+
+
+
+
     return (
         <Link>
         
-            <div className="card bg-sky-300 " style={{}}>
+            <div className="card " style={{}}>
                 <div>
-                    <img className="w-screen" src="/src/Health_Image_1.png"></img>
+                    <img className="w-screen" src={img}></img>
                 </div>
             
-                <div className="px-2 py-4" >
-                    <button className=" text-left bg-slate-400 w-[70px] public/Health_Image_1.png public/Health_Image_2.png rounded-md px-3 py-1 my-2 ml-3 mt-2" style={{}}>Health</button>
-                    <h2 className="ml-3 text-xl font-semibold" style={{}}>Clean water for children</h2>
+                <div className="px-2 py-4" style={{backgroundColor:cardColorBg}}>
+                    <button className=" text-left bg-slate-400 max-w-fit public/Health_Image_1.png public/Health_Image_2.png rounded-md px-3 py-1 my-2 ml-3 mt-2" style={{backgroundColor:categoryBg, color:textColors}}>{category}</button>
+                    <h2 className="ml-3 text-xl font-semibold" style={{color:textColors}}>{title}</h2>
                
                 </div>
             </div>
