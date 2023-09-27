30eb978b2df9e44686918f09283cf57b38ce4c16
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2023-09-27 12:30:15 +0600

    all features added dynamically for cards description

--- src/backupAI3333333.js
+++ src/backupAI3333333.js
@@ -12,14 +12,14 @@ const CardDescription = ({ newData }) => {
                 {/* The overlay */}
                 <div className=" absolute bottom-0 left-[254px] lg:w-[1400px] lg:h-[130px] bg-opacity-50 bg-black">
                     {/* Inside the overlay */}
-                    <button onClick={{}} className="  hover:bg-[#d0290c] lg:mt-[37px] ml-[] lg:ml-[37px] rounded-[4px] lg:w-[179px] lg:h-[56px] bg-[#FF444A] text-[16px] md:text-[20px] lg:text-[20px] font-medium md:font-semibold  lg:font-semibold px-4 py-2 lg:px-4 lg:py-2 text-white" type="submit">Donate ${newData.price}</button>
+                    <button onClick={{}} className=" hover:bg-[#d0290c] lg:mt-[37px] lg:ml-[37px] rounded-[4px] lg:w-[179px] lg:h-[56px] bg-[#FF444A] text-[16px] lg:text-[20px] font-semibold px-4 py-2 lg:px-4 lg:py-2 text-white" type="submit">Your Button</button>
                 </div>
                 
             </div>
 
             <div className="lg:px-64 lg:pb-20 pb-10 lg:pt-0">
-                <h1 className=" text-center md:text-left lg:text-left lg:mt-14 mt-6 text-[30px] lg:text-[40px] font-bold text-[#0B0B0B]">{newData.title}</h1>
-                <p className="lg:mt-6 mt-4 text-[18px] text-[#0B0B0B] font-normal">{newData.description}</p>
+                <h1 className="lg:mt-14 mt-6 text-[30px] lg:text-[40px] font-bold text-[#0B0B0B]">Good Education</h1>
+                <p className="lg:mt-6 mt-4 text-[18px] text-[#0B0B0B] font-normal">There are many things that can be done to ensure that all people have access to a good education. Governments can invest in public schools, provide financial assistance to students, and make sure that all schools have qualified teachers and resources. Families can support their children's education by creating a learning environment at home and helping them with their schoolwork. Teachers can create a positive and supportive learning environment for their students and challenge them to reach their full potential.</p>
 
             </div>
 
